04152eb55701e23d0ca355c6650db6f9194fb746
MrPrince419 <uwagboe.prince@gmail.com>
2025-01-22 22:31:23 -0500

    Simplify Vite and package.json configurations to match standard template

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: resolve(__dirname, '.'), // explicitly set root
-  base: '/',
   build: {
-    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
@@ -17,7 +15,6 @@ export default defineConfig({
     emptyOutDir: true, // clean the output directory before building
   },
   server: {
-    port: 3000,
     open: '/index.html', // auto-open this file in the browser
-  }
+  },
 });
